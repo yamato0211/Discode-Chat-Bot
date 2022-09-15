cf43b8b5396ff5b595b3e5683dee790624fe5ab2
yamato0211 <kumayama0211@gmail.com>
2022-09-15 14:56:53 +0900

    [add] TalkAPIの実装

---
+++ TalkAPI.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+DEFAULT_REQUEST_URL = "https://api.a3rt.recruit.co.jp/talk/v1/smalltalk"
+API_KEY = os.environ['API_KEY']
+
+def Talk_API(talk_context):
+    data = {
+        "apikey": API_KEY,
+        "query": str(talk_context),
+    }
+    result = requests.post(DEFAULT_REQUEST_URL,data=data)
+    return result
+
